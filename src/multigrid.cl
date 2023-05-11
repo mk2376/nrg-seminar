c6af482abd0851ef4c504845654fffa2ee549f80
mk2376 <mk2376@student.uni-lj.si>
2023-05-11 20:24:11 +0200

    Added restrict and prolong

--- src/multigrid.cl
+++ src/multigrid.cl
@@ -8,35 +8,35 @@ __kernel void relax_kernel(__global float* u, __global float* f, __global float*
         float u_new_value = 0.25f * (u[(x-1)*M + y] + u[(x+1)*M + y] + u[x*M + (y-1)] + u[x*M + (y+1)] - f[x*M + y]);
         unew[x*M + y] = u_new_value;
     } else {
-        unew[x*M + y] = unew[x*M + y];
+        unew[x*M + y] = u[x*M + y];
     }
 }
 
 // OpenCL kernel to perform restriction step
-__kernel void restrict_kernel(__global float* r, __global float* r2, int N) {
+__kernel void restrict_kernel(__global float* r, __global float* r2, int N, int M) {
     // Calculate the current thread index
     int x = get_global_id(0);
     int y = get_global_id(1);
 
     // Check if the thread index is within the valid range
-    if (x > 0 && x < N - 1 && y > 0 && y < N - 1) {
+    if (x > 0 && x < N - 1 && y > 0 && y < M - 1) {
         // Perform the restriction operation by averaging four neighboring values
-        r2[(y / 2) * (N / 2) + (x / 2)] = 0.25f * (r[(y - 1) * N + x] + r[(y + 1) * N + x] + r[y * N + x - 1] + r[y * N + x + 1]);
+        r2[(y / 2) * (M / 2) + (x / 2)] = 0.25f * (r[(y - 1) * N + x] + r[(y + 1) * N + x] + r[y * N + x - 1] + r[y * N + x + 1]);
     }
 }
 
 // OpenCL kernel to perform prolongation step
-__kernel void prolong_kernel(__global float* u, __global float* u2, int N) {
+__kernel void prolong_kernel(__global float* u, __global float* u2, int N, int M) {
     // Calculate the current thread index
     int x = get_global_id(0);
     int y = get_global_id(1);
 
     // Check if the thread index is within the valid range
-    if (x > 0 && x < N - 1 && y > 0 && y < N - 1) {
+    if (x > 0 && x < N - 1 && y > 0 && y < M - 1) {
         // Perform the prolongation operation by linear interpolation
-        u2[y * 2 * N + x * 2] = 0.5f * (u[(y - 1) * N + x] + u[y * N + x]);
-        u2[(y * 2 + 1) * N + x * 2] = 0.5f * (u[y * N + x] + u[(y + 1) * N + x]);
-        u2[y * 2 * N + x * 2 + 1] = 0.5f * (u[y * N + x] + u[y * N + x + 1]);
-        u2[(y * 2 + 1) * N + x * 2 + 1] = 0.5f * (u[(y + 1) * N + x] + u[y * N + x + 1]);
+        u2[y * 2 * M + x * 2] = 0.5f * (u[(y - 1) * N + x] + u[y * N + x]);
+        u2[(y * 2 + 1) * M + x * 2] = 0.5f * (u[y * N + x] + u[(y + 1) * N + x]);
+        u2[y * 2 * M + x * 2 + 1] = 0.5f * (u[y * N + x] + u[y * N + x + 1]);
+        u2[(y * 2 + 1) * M + x * 2 + 1] = 0.5f * (u[(y + 1) * N + x] + u[y * N + x + 1]);
     }
 }
--- src/multigrid.py
+++ src/multigrid.py
@@ -60,9 +60,7 @@ def load_image(filename):
 
 # Function to save the image
 def save_image(filename, image):
-    print("save_image_pre", np.min(image), np.max(image), np.mean(image))  # Print min, max and mean values
-    
-    print("save_image_post", np.min(image), np.max(image), np.mean(image))  # Print min, max and mean values
+    print("save_image", np.min(image), np.max(image), np.mean(image))  # Print min, max and mean values
     
     # Convert image to 8-bit integer format
     image = (image * 255).astype(np.uint8)
@@ -71,8 +69,20 @@ def save_image(filename, image):
 # Function to perform relaxation in the multigrid method
 def relax_kernel(program, queue, u, f, unew, N, M):
     global_work_size = (N, M)
-    local_work_size = (2, 2)  # or whatever value is appropriate for your hardware
+    local_work_size = None # (2, 2)  # or whatever value is appropriate for your hardware
     program.relax_kernel(queue, global_work_size, local_work_size, u.data, f.data, unew.data, N, M)
+
+# Function to perform restriction in the multigrid method
+def restrict_kernel(program, queue, r, r2, N, M):
+    global_work_size = (N, M)
+    local_work_size = None # (2, 2) # or whatever value is appropriate for your hardware
+    program.restrict_kernel(queue, global_work_size, local_work_size, r.data, r2.data, N, M)
+
+# Function to perform prolongation in the multigrid method
+def prolong_kernel(program, queue, u, u2, N, M):
+    global_work_size = (N, M)
+    local_work_size = None # (2, 2)  # or whatever value is appropriate for your hardware
+    program.prolong_kernel(queue, global_work_size, local_work_size, u.data, u2.data, np.int32(N), np.int32(M))
 
 # Main function
 def main():
@@ -86,51 +96,45 @@ def main():
 
     # Read image and initialize parameters
     input_image = load_image(args.input_file)
-    print("input_image", np.min(input_image), np.max(input_image), np.mean(input_image))  # Print min, max and mean values
     N = np.int32(input_image.shape[0])
     M = np.int32(input_image.shape[1])
-    print("NxM", N, M)
     num_cycles = 1
-    num_relaxations = 100
+    num_relaxations = 10
 
     # OpenCL context
     context = cl.create_some_context()
     queue = cl.CommandQueue(context)
-    device = context.devices[0]
-    max_work_group_size = device.get_info(cl.device_info.MAX_WORK_GROUP_SIZE)
-    print('Max work group size:', max_work_group_size)
-    
+
     # Allocate memory
     u = cl_array.to_device(queue, np.zeros((N, M), dtype=np.float32))
     f = cl_array.to_device(queue, generate_divergence_field(input_image).astype(np.float32))  # Convert to float32
     unew = cl_array.to_device(queue, np.zeros((N, M), dtype=np.float32))
-
-
-    # Check if the shapes of u and f are the same
-    print("Shape of u:", u.shape)
-    print("Shape of f:", f.shape)
-    
-    print("u pre", np.min(u.get()), np.max(u.get()), np.mean(u.get()))
+    r = cl_array.to_device(queue, np.zeros((N, M), dtype=np.float32))  # For restriction
+    r2 = cl_array.to_device(queue, np.zeros((N//2, M//2), dtype=np.float32))  # For restriction
+    u2 = cl_array.to_device(queue, np.zeros((N*2, M*2), dtype=np.float32))  # For prolongation
 
     # Load kernel
     with open('src/multigrid.cl', 'r') as file_obj:
         fstr = "".join(file_obj.readlines())
     program = cl.Program(context, fstr).build()
 
-    # Check values in f array
-    print("f", np.min(f.get()), np.max(f.get()), np.mean(f.get()))
-
     # Run multigrid
     for cycle in range(num_cycles):
         for relax in range(num_relaxations):
-            # print(f.get())
-            # print(f.dtype)
             relax_kernel(program, queue, u, f, unew, N, M)
-            # print("unew post", np.min(unew.get()), np.max(unew.get()), np.mean(unew.get()))
             u, unew = unew, u
-        
-        # Check values in u array
-        # print("u post", np.min(u.get()), np.max(u.get()), np.mean(u.get()))
+
+        # Apply restriction
+        restrict_kernel(program, queue, u, r2, N, M)
+        u, r2 = r2, u  # Downsample to the next grid level
+
+        # Perform relaxation on the coarser grid...
+
+        # Apply prolongation
+        prolong_kernel(program, queue, u, u2, N//2, M//2)
+        u, u2 = u2, u  # Upsample back to the original grid level
+
+        # Perform relaxation on the original grid...
 
     # Save output image
     output_image = u.get()
